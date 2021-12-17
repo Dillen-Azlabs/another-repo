38b899d54083ede01d09429c59188555d20cc3ba
Zaahid <mzra@ic.sg>
2021-12-17 09:32:26 +0700

    Update Clinic Interest to Clinical Interest

--- src/main/java/sg/ihh/ms/sdms/app/rest/model/ClinicalInterestListResponse.java
+++ src/main/java/sg/ihh/ms/sdms/app/rest/model/ClinicalInterestListResponse.java
@@ -2,35 +2,35 @@ package sg.ihh.ms.sdms.app.rest.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.HttpStatus;
-import sg.ihh.ms.sdms.app.model.ClinicInterest;
+import sg.ihh.ms.sdms.app.model.ClinicalInterest;
 
 
 import java.util.List;
 
-public class ClinicInterestListResponse extends BaseResponse{
-    @JsonProperty("clinicInterest")
-    private List<ClinicInterest> list;
+public class ClinicalInterestListResponse extends BaseResponse{
+    @JsonProperty("clinicalInterest")
+    private List<ClinicalInterest> list;
     @JsonProperty("count")
     private int count;
 
 
-    public ClinicInterestListResponse(List<ClinicInterest> list) {
+    public ClinicalInterestListResponse(List<ClinicalInterest> list) {
         super(HttpStatus.OK);
         this.count = list.size();
         this.list = list;
     }
 
-    public ClinicInterestListResponse(List<ClinicInterest> list, int count) {
+    public ClinicalInterestListResponse(List<ClinicalInterest> list, int count) {
         super(HttpStatus.OK);
         this.count = count;
         this.list = list;
     }
 
-    public List<ClinicInterest> getList() {
+    public List<ClinicalInterest> getList() {
         return list;
     }
 
-    public void setList(List<ClinicInterest> list) {
+    public void setList(List<ClinicalInterest> list) {
         this.list = list;
     }
 
