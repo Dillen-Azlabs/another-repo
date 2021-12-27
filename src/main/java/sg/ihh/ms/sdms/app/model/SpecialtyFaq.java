5d74489aeb725cd0db3d44f64cf583cf9bf013d9
Muhammad  Zaahid <mzra@ic.sg>
2021-12-27 00:49:55 +0000

    Implement MS API - Specialty FAQ - Bug Fix

--- src/main/java/sg/ihh/ms/sdms/app/model/SpecialtyFaq.java
+++ src/main/java/sg/ihh/ms/sdms/app/model/SpecialtyFaq.java
@@ -3,15 +3,15 @@ package sg.ihh.ms.sdms.app.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"uid", "languageCode", "additionalResources", "question", "answer", "faqMetaTitle", "faqMetaDescription", "order", "publishFlag", "createdDt", "modifiedDt"})
+import java.util.List;
+
+@JsonPropertyOrder({"uid", "languageCode", "additionalResources", "faqs", "faqMetaTitle", "faqMetaDescription", "order", "publishFlag", "createdDt", "modifiedDt"})
 public class SpecialtyFaq extends BaseModel{
 
     @JsonProperty("additionalResources")
     private String additionalResource;
-    @JsonProperty("question")
-    private String question;
-    @JsonProperty("answer")
-    private String answer;
+    @JsonProperty("faqs")
+    private List<SpecialtySdFaq> faqs;
     @JsonProperty("faqMetaTitle")
     private String faqTitle;
     @JsonProperty("faqMetaDescription")
@@ -29,22 +29,6 @@ public class SpecialtyFaq extends BaseModel{
         this.additionalResource = additionalResource;
     }
 
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
     public String getFaqTitle() {
         return faqTitle;
     }
@@ -59,5 +43,13 @@ public class SpecialtyFaq extends BaseModel{
 
     public void setFaqDesc(String faqDesc) {
         this.faqDesc = faqDesc;
+    }
+
+    public List<SpecialtySdFaq> getFaqs() {
+        return faqs;
+    }
+
+    public void setFaqs(List<SpecialtySdFaq> faqs) {
+        this.faqs = faqs;
     }
 }
