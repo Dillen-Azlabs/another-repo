ca745822336c5506a70b9b336469e28dff23fcce
Muhammad  Zaahid <mzra@ic.sg>
2022-02-21 04:30:31 +0000

    Resolve "Implement MS API - Get Content Hub (M) - Basic Details"

---
+++ src/main/java/sg/ihh/ms/sdms/app/repository/ContentHubMainSdRepository.java
@@ -0,0 +1,80 @@
+package sg.ihh.ms.sdms.app.repository;
+
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.statement.Query;
+import org.springframework.stereotype.Repository;
+import sg.ihh.ms.sdms.app.model.ContentHubMainBasicDetail;
+import sg.ihh.ms.sdms.app.model.Version;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ContentHubMainSdRepository extends BaseRepository {
+
+    public ContentHubMainSdRepository() {
+        log = getLogger(this.getClass());
+    }
+
+    //START - Content Hub Main Basic Detail Block
+    public ContentHubMainBasicDetail getContentHubMainBasicDetail(Version version, List<String> languageList, String contentHubMUrl, String hospitalCode) {
+        final String methodName = "getContentHubMainBasicDetail";
+        start(methodName);
+
+        String sql = "SELECT chms.*, chmsm.social_summary FROM content_hub_main_sd chms " +
+                "LEFT JOIN content_hub_main_sd_metadata chmsm  ON chms.uid = chmsm.content_hub_main_sd_uid   " +
+                "WHERE chms.language_code IN(<languageList>) AND chms.item_url = :item_url " +
+                "AND chms.publish_flag = {PUBLISHED}";
+
+        sql = getPublishVersion(version, sql);
+
+        ContentHubMainBasicDetail result = null;
+        try (Handle h = getHandle(); Query query = h.createQuery(sql)) {
+            query.bindList("languageList", languageList).bind("item_url", contentHubMUrl);
+            result = query.mapToBean(ContentHubMainBasicDetail.class).one();
+
+        } catch (Exception ex) {
+            log.error(methodName, ex);
+        }
+
+        if (result != null) {
+            Map<String, Object> metadataDetails = getMetadataBasicDetail(version, languageList, contentHubMUrl, hospitalCode);
+            if (metadataDetails.get("hospital_main_image") != null && !metadataDetails.get("hospital_main_image").equals("")) {
+                result.setMainImage((String) metadataDetails.get("hospital_main_image"));
+            }
+            if (metadataDetails.get("hospital_main_image_alt_text") != null && !metadataDetails.get("hospital_main_image_alt_text").equals("")) {
+                result.setMainImageAltText((String) metadataDetails.get("hospital_main_image_alt_text"));
+            }
+            result.setMetaTitle((String) metadataDetails.get("meta_title"));
+            result.setMetaDescription((String) metadataDetails.get("meta_description"));
+        }
+
+        completed(methodName);
+        return result;
+    }
+    public Map<String, Object> getMetadataBasicDetail(Version version, List<String> languageList, String contentHubMUrl, String hospitalCode) {
+        final String methodName = "getMetadataBasicDetail";
+        start(methodName);
+
+        String sql = "SELECT chmsm.hospital_main_image, chmsm.hospital_main_image_alt_text, chmsm.social_summary, chmsm.meta_title, chmsm.meta_description  FROM content_hub_main_sd chms " +
+                "LEFT JOIN content_hub_main_sd_metadata chmsm  ON chms.uid = chmsm.content_hub_main_sd_uid " +
+                "LEFT JOIN hospital h ON chmsm.hospital_uid  = h.uid " +
+                "WHERE chms.language_code IN(<languageList>) AND chms.item_url = :item_url AND h.hospital = :hospital " +
+                "AND chms.publish_flag = {PUBLISHED}";
+
+        sql = getPublishVersion(version, sql);
+
+        Map<String, Object> result = new HashMap<>();
+        try (Handle h = getHandle(); Query query = h.createQuery(sql)) {
+            query.bindList("languageList", languageList).bind("item_url", contentHubMUrl).bind("hospital", hospitalCode);
+            result = query.mapToMap().one();
+
+        } catch (Exception ex) {
+            log.error(methodName, ex);
+        }
+        completed(methodName);
+        return result;
+    }
+    //END - Content Hub Main Basic Detail Block
+}
---
+++ src/main/java/sg/ihh/ms/sdms/app/rest/service/ContentHubMainSdService.java
@@ -0,0 +1,54 @@
+package sg.ihh.ms.sdms.app.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import sg.ihh.ms.sdms.app.model.ContentHubMainBasicDetail;
+import sg.ihh.ms.sdms.app.model.Version;
+import sg.ihh.ms.sdms.app.processor.StructuredDataProcessor;
+import sg.ihh.ms.sdms.app.repository.ContentHubMainSdRepository;
+import sg.ihh.ms.sdms.app.rest.model.ContentHubMainBasicDetailListResponse;
+
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "contentHubM", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+@Validated
+public class ContentHubMainSdService extends BaseService{
+
+    @Autowired
+    private ContentHubMainSdRepository repository;
+
+    @Autowired
+    private StructuredDataProcessor<ContentHubMainBasicDetail> bdprocessor;
+
+    public ContentHubMainSdService() {
+        log = getLogger(this.getClass());
+    }
+
+    //START - Content Hub Main Basic Detail Block
+    @RequestMapping(path = "details", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ContentHubMainBasicDetailListResponse getContentHubMBasicDetail(
+            @RequestParam("version") @Pattern(regexp = "^(DRAFT|PUBLISHED)$", message = "Allowed Values : DRAFT, PUBLISHED") String version,
+            @RequestParam("languageCode") String languageCode,
+            @RequestParam("contentHubMUrl") String contentHubMUrl,
+            @RequestParam("hospitalCode") String hospitalCode) {
+        final String methodName = "getContentHubMBasicDetail";
+        start(methodName);
+
+        // Language Code
+        List<String> languageList = getLanguageList(languageCode);
+
+        ContentHubMainBasicDetail result = repository.getContentHubMainBasicDetail(Version.getVersion(version), languageList, contentHubMUrl,hospitalCode);
+
+        ContentHubMainBasicDetailListResponse response = new ContentHubMainBasicDetailListResponse(result);
+
+        completed(methodName);
+        return response;
+    }
+    //END - Content Hub Main Basic Detail Block
+}
