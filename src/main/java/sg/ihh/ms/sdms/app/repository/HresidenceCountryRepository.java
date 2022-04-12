96418b223b1c98812182b8705c52a2b37d056d77
hariz.lutfi <hariz.lutfi@HARIZ-LAPTOP.az-ex.sg>
2022-04-12 17:08:42 +0800

    Setting up ResidenceCountryRepository

---
+++ src/main/java/sg/ihh/ms/sdms/app/repository/HresidenceCountryRepository.java
@@ -0,0 +1,13 @@
+package sg.ihh.ms.sdms.app.repository;
+
+public class HresidenceCountryRepository extends BaseRepository {
+
+	
+	public HresidenceCountryRepository()
+	{
+        log = getLogger(this.getClass());
+        tableName = "country_residence";
+        
+	}
+	
+}
