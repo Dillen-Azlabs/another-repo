823ca2929474afd6e166dc2882b8d54f24e0109d
supporic <support@ic.sg>
2022-04-13 13:30:31 +0800

    HresidenceCountry

---
+++ src/main/java/sg/ihh/ms/sdms/app/model/HresidenceCountry.java
@@ -0,0 +1,29 @@
+package sg.ihh.ms.sdms.app.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"uid","language","cor","order","publishFlag","createdDt", "modifiedDt"})
+public class HresidenceCountry extends BaseModel{
+
+	
+	@JsonProperty("cor")
+	private String cor;
+	
+	public HresidenceCountry()
+	{
+		//EmptyConstructor
+	}
+	
+	public String getCor()
+	{
+		return cor;
+	}
+	
+	public void setGender(String cor)
+	{
+	    this.cor = cor;
+	}
+	
+	
+}
