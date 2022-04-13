314d271b59e28b2021782cbe89344c1b153c9e3b
supporic <support@ic.sg>
2022-04-13 17:18:58 +0800

    Updated and working Repository file

--- src/main/java/sg/ihh/ms/sdms/app/repository/HresidenceCountryRepository.java
+++ src/main/java/sg/ihh/ms/sdms/app/repository/HresidenceCountryRepository.java
@@ -2,22 +2,24 @@ package sg.ihh.ms.sdms.app.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import sg.ihh.ms.sdms.app.model.Gender;
 import sg.ihh.ms.sdms.app.model.HresidenceCountry;
 import sg.ihh.ms.sdms.app.model.Version;
 
+@Repository
 public class HresidenceCountryRepository extends BaseRepository {
 
 	
 	public HresidenceCountryRepository()
 	{
         log = getLogger(this.getClass());
-        tableName = "country_residence";
+        tableName = "country_of_residence";
         
 	}
 	 public List<HresidenceCountry> list(Version version, List<String> languageList) {
 	        return super.list(version, languageList, HresidenceCountry.class);
 	    }
-	
-	
+
 }
