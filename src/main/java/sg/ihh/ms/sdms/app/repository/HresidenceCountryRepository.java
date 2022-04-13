fa7e513815cf1a52101e2cbb2a70ded841971fa3
supporic <support@ic.sg>
2022-04-13 13:28:06 +0800

    set log in constructor
    set list

--- src/main/java/sg/ihh/ms/sdms/app/repository/HresidenceCountryRepository.java
+++ src/main/java/sg/ihh/ms/sdms/app/repository/HresidenceCountryRepository.java
@@ -1,5 +1,11 @@
 package sg.ihh.ms.sdms.app.repository;
 
+import java.util.List;
+
+import sg.ihh.ms.sdms.app.model.Gender;
+import sg.ihh.ms.sdms.app.model.HresidenceCountry;
+import sg.ihh.ms.sdms.app.model.Version;
+
 public class HresidenceCountryRepository extends BaseRepository {
 
 	
@@ -8,7 +14,10 @@ public class HresidenceCountryRepository extends BaseRepository {
         log = getLogger(this.getClass());
         tableName = "country_residence";
         
-        
 	}
+	 public List<HresidenceCountry> list(Version version, List<String> languageList) {
+	        return super.list(version, languageList, HresidenceCountry.class);
+	    }
+	
 	
 }
