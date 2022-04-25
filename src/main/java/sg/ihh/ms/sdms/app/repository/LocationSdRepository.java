203aac90f7af7743f8727162f87a0f6e2721f66e
Muhammad  Zaahid <mzra@ic.sg>
2022-04-25 09:29:55 +0000

    Resolve "Implement MS API - Get Locations by Item URLs"

--- src/main/java/sg/ihh/ms/sdms/app/repository/LocationSdRepository.java
+++ src/main/java/sg/ihh/ms/sdms/app/repository/LocationSdRepository.java
@@ -22,13 +22,14 @@ public class LocationSdRepository  extends BaseRepository {
     public List<LocationSd> getLocationByItemUrl(Version version, List<String> languageList, List<String> itemUrls, String hospitalCode){
         final String methodName = "getLocationByItemUrl";
         start(methodName);
-        String sql ="SELECT ls.uid, ls.language_code, ls.location_title, ls.address1, ls.address2, ls.city , ls.state, ls.postal_code, cor.cor , ls.whatsapp_number, ls.fax, ls.email, ls.publish_flag, ls.created_dt, ls.modified_dt FROM location_sd ls " +
+        String sql ="SELECT ls.uid, ls.language_code, ls.location_title, ls.address1, ls.address2, ls.city , ls.state, ls.postal_code, cor.cor , ls.whatsapp_number, ls.fax, ls.email, ls.display_order, ls.publish_flag, ls.created_dt, ls.modified_dt FROM location_sd ls " +
                 "LEFT JOIN location_sd_contact lsc  ON ls.uid = lsc.location_sd_uid " +
                 "LEFT JOIN country_of_residence cor ON ls.cor_uid  = cor.uid " +
                 "LEFT JOIN location_sd_hospital lsh ON ls.uid  = lsh.location_sd_uid " +
                 "LEFT JOIN hospital h ON lsh.hospital_uid  = h.uid " +
                 "WHERE ls.language_code IN(<languageList>) AND ls.item_url IN(<itemUrls>) AND h.hospital = :hospital " +
-                "AND ls.publish_flag = {PUBLISHED}";
+                "AND ls.publish_flag = {PUBLISHED} " +
+                "GROUP BY ls.uid ";
 
         sql = getPublishVersion(version, sql);
 
@@ -59,7 +60,8 @@ public class LocationSdRepository  extends BaseRepository {
                 "LEFT JOIN location_sd_hospital lsh ON ls.uid  = lsh.location_sd_uid " +
                 "LEFT JOIN hospital h ON lsh.hospital_uid  = h.uid " +
                 "WHERE ls.language_code IN(<languageList>) AND ls.item_url IN(<itemUrls>) AND h.hospital = :hospital " +
-                "AND ls.publish_flag = {PUBLISHED}";
+                "AND ls.publish_flag = {PUBLISHED} " +
+                "GROUP BY ls.uid ";
 
         sql = getPublishVersion(version, sql);
 
