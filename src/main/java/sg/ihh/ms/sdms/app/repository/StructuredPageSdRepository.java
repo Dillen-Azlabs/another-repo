5d014b62fe822ec00282ff00bad609d122a509a4
TSO <TSO@IC.SG>
2022-04-26 12:50:35 +0800

    Fixed query for multiple tab sections

--- src/main/java/sg/ihh/ms/sdms/app/repository/StructuredPageSdRepository.java
+++ src/main/java/sg/ihh/ms/sdms/app/repository/StructuredPageSdRepository.java
@@ -573,7 +573,7 @@ public class StructuredPageSdRepository extends BaseRepository {
         final String methodName = "getStructuredPageTab";
         start(methodName);
 
-        String sql = " SELECT spsts.tab_section_intro as sectionIntro FROM structured_page_sd sps " +
+        String sql = " SELECT spsts.*, spsts.tab_section_intro as sectionIntro FROM structured_page_sd sps " +
                 " LEFT JOIN structured_page_sd_tab_section spsts ON sps.uid = spsts.structured_page_sd_uid " +
                 " LEFT JOIN structured_page_sd_tab_section_hospital spstsh ON spsts.uid = spstsh.structured_page_sd_tab_section_uid " +
                 " LEFT JOIN hospital h ON h.uid = spstsh.hospital_uid " +
