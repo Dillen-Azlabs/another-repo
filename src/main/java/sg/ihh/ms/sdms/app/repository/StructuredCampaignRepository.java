d63af25c5184547064b748a8356d4eca6c94176a
Muhammad Fatih Farkhan Hasani <mffh@ic.sg>
2022-05-18 02:59:01 +0000

    Resolve "SDMS - Implement MS API - Get Structured Campaign Accordion List"

--- src/main/java/sg/ihh/ms/sdms/app/repository/StructuredCampaignRepository.java
+++ src/main/java/sg/ihh/ms/sdms/app/repository/StructuredCampaignRepository.java
@@ -4,10 +4,11 @@ package sg.ihh.ms.sdms.app.repository;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.statement.Query;
 import org.springframework.stereotype.Repository;
+import sg.ihh.ms.sdms.app.model.StructuredCampaignAccordion;
 import sg.ihh.ms.sdms.app.model.StructuredCampaignDetails;
-import sg.ihh.ms.sdms.app.model.StructuredPageBasicDetail;
 import sg.ihh.ms.sdms.app.model.Version;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -44,4 +45,36 @@ public class StructuredCampaignRepository extends BaseRepository {
 
         return structuredCampaignDetails;
     }
+
+    // END - Structured Campaign Basic Details
+
+    // START - Structured Campaign Accordion
+    public List<StructuredCampaignAccordion> getStructuredCampaignAccordion(Version version, List<String> languageList, String structuredCampaignUrl, String hospitalCode){
+        final String methodName = "getStructuredCampaignAccordion";
+        start(methodName);
+
+        String sql = " SELECT scsa.* FROM structured_campaign_sd scs " +
+                "LEFT JOIN structured_campaign_sd_accordion scsa ON scs.uid = scsa.structured_campaign_sd_uid " +
+                "LEFT JOIN structured_campaign_sd_hospital scsh ON scs.uid  = scsh.structured_campaign_sd_uid  " +
+                "LEFT JOIN hospital h ON h.uid = scsh.hospital_uid " +
+                "WHERE scs.language_code IN(<languageList>) AND scs.item_url = :item_url AND h.hospital = :hospital " +
+                "AND scs.publish_flag = {PUBLISHED} ";
+
+        sql = getPublishVersion(version, sql);
+
+        List<StructuredCampaignAccordion> result = new ArrayList<>();
+        try (Handle h = getHandle(); Query query = h.createQuery(sql)) {
+            query.bindList("languageList", languageList).bind("item_url", structuredCampaignUrl).bind("hospital", hospitalCode);
+            result = query.mapToBean(StructuredCampaignAccordion.class).list();
+
+        } catch (Exception ex) {
+            log.error(methodName, ex);
+        }
+
+        completed(methodName);
+
+        return result;
+    }
+    // END - Structured Campaign Accordion
+
 }
